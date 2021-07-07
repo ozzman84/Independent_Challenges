bc39d71fce16ff597e98ee0a8873d09693076e6d
Ozzie <mikeosmonson@gmail.com>
2021-07-06 19:16:09 -0600

    Add iteration 1

--- ic/seattle_grace_2105/spec/doctor_spec.rb
+++ ic/seattle_grace_2105/spec/doctor_spec.rb
@@ -1,13 +1,16 @@
 require './lib/doctor'
 
 RSpec.describe Doctor do
-  it 'exists & has attributes' do
-    meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
 
-    expect(meredith).to be_a(Doctor)
-    expect(meredith.name).to eq("Meredith Grey")
-    expect(meredith.specialty).to eq("General Surgery")
-    expect(meredith.education).to eq("Harvard University")
-    expect(meredith.salary).to eq(100000)
+  before(:each) do
+    @meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+  end
+
+  it 'exists & has attributes' do
+    expect(@meredith).to be_a(Doctor)
+    expect(@meredith.name).to eq("Meredith Grey")
+    expect(@meredith.specialty).to eq("General Surgery")
+    expect(@meredith.education).to eq("Harvard University")
+    expect(@meredith.salary).to eq(100000)
   end
 end
