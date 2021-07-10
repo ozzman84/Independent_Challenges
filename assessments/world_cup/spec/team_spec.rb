f8dd65b1da57f9d10898ee2e91efd89e6cacf8d5
Ozzie <mikeosmonson@gmail.com>
2021-07-10 17:47:00 -0600

    Refactor

--- assessments/world_cup/spec/team_spec.rb
+++ assessments/world_cup/spec/team_spec.rb
@@ -4,11 +4,8 @@ require './lib/player'
 RSpec.describe Team do
   before(:each) do
     @mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
-    # => #<Player:0x00007fe0d02bd280...>
-    @pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
-    # => #<Player:0x00007fe0d0851138...>
-    @team = Team.new("France")
-    # => #<Team:0x00007fe0d0335d48...>
+    @pogba  = Player.new({name: "Paul Pogba", position: "midfielder"})
+    @team   = Team.new("France")
   end
 
   it 'exists & has attributes' do
@@ -20,14 +17,12 @@ RSpec.describe Team do
     expect(@team.eliminated?).to eq(false)
     @team.eliminated
     expect(@team.eliminated?).to eq(true)
-
   end
 
   it 'returns & adds players' do
     expect(@team.players).to eq([])
 
     @team.add_player(@mbappe)
-
     @team.add_player(@pogba)
 
     expect(@team.players).to eq([@mbappe, @pogba])
